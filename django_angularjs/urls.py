732fd16b2df23f69456fd0f2410835fd181bf85c
almeidajeff <jralmeida88@gmail.com>
2015-11-16 14:37:03 -0200

    API -v1

--- django_angularjs/urls.py
+++ django_angularjs/urls.py
@@ -1,9 +1,18 @@
-from django.conf.urls import patterns, url
+# -*- coding:utf-8 -*-
+# Imports
+from rest_framework_nested import routers
 
+from authentication.views import AccountViewSet
+
+from django.conf.urls import patterns, url
 from django_angularjs.views import IndexView
+
+router = routers.SimpleRouter()
+router.register(r'accounts', AccountViewSet)
 
 urlpatterns = patterns(
     '',
-
+    # URLs
+    url(r'^api/v1/', include(router.urls)),
     url('^.*$', IndexView.as_view(), name='index'),
 )
