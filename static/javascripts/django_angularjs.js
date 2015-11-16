92091d4519929cec40fb1aec4a3d0571e2f81357
almeidajeff <jralmeida88@gmail.com>
2015-11-16 15:17:06 -0200

    Construindo a interface básica de registro dos usuários

--- static/javascripts/django_angularjs.js
+++ static/javascripts/django_angularjs.js
@@ -1,2 +1,3 @@
+/* O AngularJS suporta o uso de módulos. Modularização é uma grande característica, porque promove encapsulamento e acoplamento fraco. */
 angular
   .module('django_angularjs', []);
